9d8de7dd3c00d725de9accb270c7be9bd771b0dd
levan <levan.bokeria@gmail.com>
2021-05-17 11:41:43 +0100

    working on cleaning up the mutate script

--- scripts/utils/mutate_results_table_columns.R
+++ scripts/utils/mutate_results_table_columns.R
@@ -24,32 +24,7 @@ mutate_results_table_columns = function(dataIn){
                 dataIn <- dataIn %>% 
                         mutate(prompt_img_name=as.factor(prompt_img_name))
         }
-        
-        # # Add a factor describing whether the arrangement has an outlier in the lower left of the space
-        # dataIn$phase_1_lower_left_outlier <- TRUE
-        # 
-        # dataIn$phase_1_lower_left_outlier[
-        #         dataIn$arr_phase_1_1 == "1"] <- TRUE
-        # dataIn$phase_1_lower_left_outlier[
-        #         dataIn$arr_phase_1_1 == "14"] <- FALSE
-        # dataIn$phase_1_lower_left_outlier[
-        #         dataIn$arr_phase_1_1 == "3"] <- FALSE
-        # dataIn$phase_1_lower_left_outlier[
-        #         dataIn$arr_phase_1_1 == "15"] <- TRUE
-        # 
-        # # Same for phase 2
-        # dataIn$phase_2_lower_left_outlier <- TRUE
-        # 
-        # dataIn$phase_2_lower_left_outlier[
-        #         dataIn$arr_phase_2_1 == "1"] <- TRUE
-        # dataIn$phase_2_lower_left_outlier[
-        #         dataIn$arr_phase_2_1 == "14"] <- FALSE
-        # dataIn$phase_2_lower_left_outlier[
-        #         dataIn$arr_phase_2_1 == "3"] <- FALSE
-        # dataIn$phase_2_lower_left_outlier[
-        #         dataIn$arr_phase_2_1 == "15"] <- TRUE        
-        # 
-        
+  
         # Reorder levels for various things
         dataIn <-
                 dataIn %>%
